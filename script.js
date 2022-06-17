d5fe096548bb13359552096fa6d3957c0f23f618
Luide Santos de Jesus <luide19santos@gmail.com>
2022-06-17 09:22:56 -0300

    Verificador
    verificador concluido

---
+++ script.js
@@ -0,0 +1,20 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+        alert('Verifique os dados e tente novamente')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'homem'
+        } else if (fsex[1].checked){
+            genero = 'mulher'
+        }
+        res.innerHTML = `Você é  ${genero} com ${idade} anos.`
+    }
+}
